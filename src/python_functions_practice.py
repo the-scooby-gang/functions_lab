6b1979f91999bac9d3e95148dfe0d8505edfcebf
Aimee Bunyan <aimeebunyan@Aimees-MacBook-Air.local>
2022-07-06 16:33:54 +0100

    Initial commit

---
+++ src/python_functions_practice.py
@@ -0,0 +1,16 @@
+def return_10():
+    return 10
+def add(number1,number2):
+    return(number1+number2)
+
+def subtract(number1, number2):
+    return(number1-number2) 
+
+def multiply(number1, number2):
+    return(number1*number2) 
+
+def divide(number1, number2):
+    return(number1/number2)
+
+ 
+
